8f43d0e54227e25cbf4caf1fb2bfa57c6f54d136
imran jeferly <imranjeferly@gmail.com>
2025-08-03 20:29:48 +0400

    fixed errors

--- src/app/api/payment/initiate/route.js
+++ src/app/api/payment/initiate/route.js
@@ -7,7 +7,7 @@ if (!getApps().length) {
   initializeApp({
     credential: cert({
       project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      client_email: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
       private_key: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
   });
